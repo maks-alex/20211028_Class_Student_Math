d974a8d552ba594ca373e77323321e456af9f0c6
Maksymikhin Oleksandr <maks.alex@gmail.com>
2021-10-28 06:18:05 +0300

    MathFunction class and Point structure added

---
+++ 20211028_Class_Student_Math/MathFunctions.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _20211028_Class_Student_Math
+{
+    public static class MathFunctions
+    {
+        public static double ExpFunction(double x)
+        {
+            return Math.Exp(x);
+        }
+
+        public static double ArcSinFunction(double x)
+        {
+            return Math.Asin(x);
+        }
+
+        public static double DistanceBetweenPoints(Point a, Point b)
+        {
+            return Math.Sqrt(Math.Pow(a.X - b.X, 2) + Math.Pow(a.Y - b.Y, 2) + Math.Pow(a.Z - b.Z, 2));
+        }
+
+        public static void MatrixTranspose(ref int[,] arr)
+        {
+            int[,] tmp = new int[arr.GetLength(dimension: 1), arr.GetLength(dimension: 0)];
+            for (int i = 0; i < arr.GetLength(dimension: 0); i++)
+            {
+                for (int j = 0; j < arr.GetLength(dimension: 1); j++)
+                {
+                    tmp[j, i] = arr[i, j];
+                }
+            }
+            arr = tmp;
+        }
+
+    }
+}
---
+++ 20211028_Class_Student_Math/Point.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _20211028_Class_Student_Math
+{
+    public struct Point
+    {
+        private int _x;
+        private int _y;
+        private int _z;
+
+        public Point(int x, int y, int z)
+        {
+            _x = x;
+            _y = y;
+            _z = z;
+        }
+        public int X { get => _x; set => _x = value; }
+        public int Y { get => _y; set => _y = value; }
+        public int Z { get => _z; set => _z = value; }
+    }
+}
